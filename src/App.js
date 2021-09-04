6718b13341b98b6443bada70e61e132eb8159ab6
Mayank sinha <mayanksinha2399@gmail.com>
2021-09-04 18:03:40 +0530

    Bootstrap import

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li>Action</li>
+        <li>Another action</li>
+        <li>Something else here</li>
+      </ul>
+    </div>
+    // <>
+    // <p>hi</p>
+    // </>
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    //     <div className="container-fluid">
+    //       {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
+    //       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
+    //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    //           <li className="nav-item" >
+    //             TODO
+    //             {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+    //           </li>
+    //           <li className="nav-item">
+    //             {/* <Link className="nav-link" to="/about">About</Link> */}
+    //           </li>  
+    //         </ul>
+    //         {/* { props.searchBar? <form className="d-flex">
+    //           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+    //           <button className="btn btn-outline-success" type="submit">Search</button>
+    //         </form>: ""} */}
+    //       </div>
+    //     </div>
+    //   </nav>
+  );
+}
+
+export default App;
